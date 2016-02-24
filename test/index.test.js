8fd4274eb22a0beba059903478312742699a3ea4
Justen <justenbarget@gmail.com>
2016-02-24 12:13:13 +0000

    fixes #12, adding functionality to fill d3 wheel on submit

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,6 @@
 QUnit.test('selecting one of the radio buttons changes the value of data retrieved from the form', function(assert) {
   var some_element = document.getElementById('ta-form');
-
   var alwaysAppearanceOthers = document.getElementsByName('appearance-others');
-
   var node = Array.prototype.slice.call(alwaysAppearanceOthers).filter(function(el){
     return el.className === 'always';
   });
@@ -12,9 +10,11 @@ QUnit.test('selecting one of the radio buttons changes the value of data retriev
   some_element.dispatchEvent(event);
 
   $(node).trigger('click');
+  var actual = getAnswers(some_element);
+  var expected = [{
+    question:'appearance-others',
+    answer:'always'
+  }];
 
-  var expected = {'appearance-others': 'always'};
-
-  assert.deepEqual(getAnswers(some_element), expected);
-
+  assert.deepEqual(actual, expected);
 });
