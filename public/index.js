65c48dfc5962e4fe158cd365c406c9674625af3d
Danielwhyte <danwhy@gmail.com>
2016-03-01 18:42:04 +0000

    adds feedback that all sections need to be filled #62

--- public/index.js
+++ public/index.js
@@ -2,15 +2,20 @@ var form = document.getElementById('ta-form');
 var wholeForm = $("#whole-form");
 var newFormButton = document.getElementById('new-form');
 var pdfButton  = $("#pdf");
-var questions = ["personal-appearance","appearance-others","likes","dislikes","strengths","identify-emotions-self","identify-emotions-others","bodily-reaction-emotions","identify-response-emotions","plan-respond-emotions","good-eye-contact","good-distance-touch","identify-expressions","body-language-self","body-language-others","good-volume","good-pace","clear-speech","speak-with-relevance","indentify-good-speech","think-before-react","understand-problems-occurred","problems-solved-appropriately","make-a-plan","follow-a-plan","built-one-friendship","built-multiple-friendships","express-thoughts-to-others","disagree-with-others","apologise-appropriately"];
+var questions = ["personal-appearance","appearance-others","likes","dislikes","strengths","identify-emotions-self","identify-emotions-others","bodily-reaction-emotions","identify-response-emotions","plan-respond-emotions","good-eye-contact","good-distance-touch","identify-expressions","body-language-self","body-language-others","good-volume","good-pace","clear-speech","speak-with-relevance","identify-good-speech","think-before-react","understand-problems-occurred","problems-solved-appropriately","make-a-plan","follow-a-plan","built-one-friendship","built-multiple-friendships","express-thoughts-to-others","disagree-with-others","apologise-appropriately"];
 
 form.addEventListener('submit', function(e){
   e.preventDefault();
   $('.unanswered').removeClass('unanswered');
+  if ($(".active")[0]){
+    location.href = '#ta-form';
+  }
+  $(".active").removeClass("active");
+  $(".section").addClass("collapsed");
+  $('.required').removeClass('hidden');
   if (allFilled()) {
+    $('.required').addClass('hidden');
     getAnswers(e.target);
-    $(".active").removeClass("active");
-    $(".section").addClass("collapsed");
     sendRequest("https://script.google.com/macros/s/AKfycbxzdgBRvWFf9CDWjZ4M8VyGlYyMwL3ScEFY9ukqw9xntvV2cQI3/exec", 'null=null&' + $(form).serialize());
     sendRequest("https://inclusive-classrooms.herokuapp.com/redis", $(form).serializeArray());
   }
